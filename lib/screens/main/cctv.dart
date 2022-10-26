ec3ababdae7fee15ad39f3a27a2a6cbf5507ed8d
v4lomyr <bintangdwitama160501@gmail.com>
2022-10-26 23:07:47 +0700

    🐱‍💻 New CCTV screen

--- lib/screens/main/cctv.dart
+++ lib/screens/main/cctv.dart
@@ -1,17 +1,25 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:gesture_zoom_box/gesture_zoom_box.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class CCTV extends StatefulWidget {
-  const CCTV({super.key});
+  const CCTV({super.key, required this.channel});
+  final WebSocketChannel channel;
 
   @override
   State<CCTV> createState() => _CCTVState();
 }
 
 class _CCTVState extends State<CCTV> {
+  final vidWidth = 640;
+  final vidHeight = 480;
+
+  double newVidSizeWidth = 480;
+  double newVidSizeHeight = 640;
+
+  bool isLandscape = false;
+
   @override
   initState() {
     super.initState();
@@ -22,7 +30,7 @@ class _CCTVState extends State<CCTV> {
         DeviceOrientation.landscapeLeft
       ]
     );
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+    isLandscape = false;
   }
 
   @override
@@ -35,9 +43,42 @@ class _CCTVState extends State<CCTV> {
 
   @override
   Widget build(BuildContext context) {
-    return const WebView(
-      initialUrl:
-      'https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1',
+    return OrientationBuilder(
+      builder: (context, orientation){
+        var screenWidth = MediaQuery.of(context).size.width;
+        var screenHeight = MediaQuery.of(context).size.height;
+
+        if (orientation == Orientation.portrait) {
+          isLandscape = false;
+          newVidSizeWidth = (screenWidth > vidWidth ? vidWidth : screenWidth) as double;
+          newVidSizeHeight = (vidHeight * newVidSizeWidth / vidWidth );
+        } else {
+          isLandscape = true;
+          newVidSizeHeight = (screenHeight > vidHeight ? vidHeight : screenHeight) as double;
+          newVidSizeWidth = (vidWidth * newVidSizeHeight / vidHeight );
+        }
+
+        return GestureZoomBox(
+          maxScale: 5.0,
+          doubleTapScale: 2.0,
+          child: StreamBuilder(
+              stream: widget.channel.stream,
+              builder: (context, AsyncSnapshot<dynamic> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  return Image.memory(
+                      snapshot.data,
+                      gaplessPlayback: true,
+                      width: newVidSizeWidth,
+                      height: newVidSizeHeight,
+                  );
+                }
+              }),
+        );
+      },
     );
   }
 }
