bce9618591cfcefc43cd574c4e350c28ab6a3bd2
v4lomyr <bintangdwitama160501@gmail.com>
2022-11-22 22:30:25 +0700

    🐱‍💻 Auto rotate CCTV player

--- lib/screens/main/cctv.dart
+++ lib/screens/main/cctv.dart
@@ -4,12 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gesture_zoom_box/gesture_zoom_box.dart';
 import 'package:intl/intl.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter_mjpeg/flutter_mjpeg.dart';
 
 class CCTV extends StatefulWidget {
   const CCTV({super.key, required this.channel});
-  // final WebSocketChannel channel;
   final String channel;
   @override
   State<CCTV> createState() => _CCTVState();
@@ -42,7 +40,6 @@ class _CCTVState extends State<CCTV> {
 
   @override
   void dispose() {
-    // widget.channel.sink.close();
     super.dispose();
   }
 
@@ -62,80 +59,55 @@ class _CCTVState extends State<CCTV> {
   @override
   Widget build(BuildContext context) {
     bool isRunning = true;
-    return Column(
-      children: [
-        Expanded(
-            child: Center(
-              child: Mjpeg(
-                isLive: isRunning,
-                stream: widget.channel ,
-              ),
-            )
-        )
-      ],
+    return OrientationBuilder(
+      builder: (context, orientation){
+        var screenWidth = MediaQuery.of(context).size.width;
+        var screenHeight = MediaQuery.of(context).size.height;
+
+        if (orientation == Orientation.portrait) {
+          isLandscape = false;
+          newVidSizeWidth = (screenWidth > vidWidth ? vidWidth : screenWidth) as double;
+          newVidSizeHeight = (vidHeight * newVidSizeWidth / vidWidth );
+        } else {
+          isLandscape = true;
+          newVidSizeHeight = (screenHeight > vidHeight ? vidHeight : screenHeight) as double;
+          newVidSizeWidth = (vidWidth * newVidSizeHeight / vidHeight );
+        }
+
+        return Container(
+          color: Colors.blueAccent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  GestureZoomBox(
+                    maxScale: 5.0,
+                    doubleTapScale: 2.0,
+                    child: Mjpeg(
+                      isLive: isRunning,
+                      stream: widget.channel ,
+                    ),
+                  ),
+                  Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Column(
+                          children: <Widget>[
+                            const SizedBox(height: 16,),
+                            const Text("Camera View", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),),
+                            const SizedBox(height: 4,),
+                            Text("Live | $_timeString", style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w300),),
+                          ],
+                        ),
+                      )
+                  )
+                ],
+              )
+            ],
+          )
+        );
+      },
     );
-    // return OrientationBuilder(
-    //   builder: (context, orientation){
-    //     var screenWidth = MediaQuery.of(context).size.width;
-    //     var screenHeight = MediaQuery.of(context).size.height;
-    //
-    //     if (orientation == Orientation.portrait) {
-    //       isLandscape = false;
-    //       newVidSizeWidth = (screenWidth > vidWidth ? vidWidth : screenWidth) as double;
-    //       newVidSizeHeight = (vidHeight * newVidSizeWidth / vidWidth );
-    //     } else {
-    //       isLandscape = true;
-    //       newVidSizeHeight = (screenHeight > vidHeight ? vidHeight : screenHeight) as double;
-    //       newVidSizeWidth = (vidWidth * newVidSizeHeight / vidHeight );
-    //     }
-    //
-    //     return Container(
-    //       color: Colors.blueAccent,
-    //       child: StreamBuilder(
-    //           stream: widget.channel.stream,
-    //           builder: (context, AsyncSnapshot<dynamic> snapshot) {
-    //             if (!snapshot.hasData) {
-    //               return const Center(
-    //                 child: CircularProgressIndicator(),
-    //               );
-    //             } else {
-    //               return Row(
-    //                 mainAxisAlignment: MainAxisAlignment.center,
-    //                 children: [
-    //                   Stack(
-    //                     children: <Widget>[
-    //                       GestureZoomBox(
-    //                         maxScale: 5.0,
-    //                         doubleTapScale: 2.0,
-    //                         child: Image.memory(
-    //                           snapshot.data,
-    //                           gaplessPlayback: true,
-    //                           width: newVidSizeWidth,
-    //                           height: newVidSizeHeight,
-    //                         ),
-    //                       ),
-    //
-    //                       Positioned.fill(
-    //                           child: Align(
-    //                             alignment: Alignment.topCenter,
-    //                             child: Column(
-    //                               children: <Widget>[
-    //                                 const SizedBox(height: 16,),
-    //                                 const Text("Camera View", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),),
-    //                                 const SizedBox(height: 4,),
-    //                                 Text("Live | $_timeString", style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w300),),
-    //                               ],
-    //                           ),
-    //                         )
-    //                       )
-    //                     ],
-    //                   ),
-    //                 ],
-    //               );
-    //             }
-    //           }),
-    //     );
-    //   },
-    // );
   }
 }
