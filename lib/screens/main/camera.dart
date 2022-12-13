8679944948406d6328a3bbe6a0c995da84f56219
v4lomyr <bintangdwitama160501@gmail.com>
2022-12-14 00:15:14 +0700

    🐱‍💻 revisi camera

--- lib/screens/main/camera.dart
+++ lib/screens/main/camera.dart
@@ -1,12 +1,11 @@
 import 'dart:developer';
 
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:seguro/screens/main/input_photo.dart';
 
 import '../../main.dart';
-
-import 'main_screen.dart';
 
 class Camera extends StatefulWidget {
   const Camera({super.key});
@@ -16,56 +15,191 @@ class Camera extends StatefulWidget {
 }
 
 class _CameraState extends State<Camera> {
-  late CameraController _controller;
+  late CameraController? _controller;
   late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
     _controller = CameraController(cameras.first, ResolutionPreset.max);
-    _initializeControllerFuture = _controller.initialize();
+    _initializeControllerFuture = _controller!.initialize();
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    _controller!.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Take a Picture')),
-      body: FutureBuilder(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return CameraPreview(_controller);
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
+        appBar: AppBar(title: const Text('Take a Picture')),
+        body: Column(
+          children: [
+            _cameraTogglesRowWidget(),
+            Expanded(
+              child: FutureBuilder(
+                  future: _initializeControllerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return CameraPreview(_controller!);
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  }),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              try {
+                await _initializeControllerFuture;
 
-            final image = await _controller.takePicture();
-            String imagePath = image.path;
+                final image = await _controller!.takePicture();
+                String imagePath = image.path;
 
-            if (!mounted) return;
+                if (!mounted) return;
 
-            await Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => InputPhoto(
-                  imageFromCamera: imagePath,
-                )
-            ));
-          } catch (e) {
-            log(e.toString());
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
+                await Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => InputPhoto(
+                      imageFromCamera: imagePath,
+                    )
+                ));
+              } catch (e) {
+                log(e.toString());
+              }
+            },
+            child: const Icon(Icons.camera_alt),
+          ),
+        );
+    // );
+  }
+
+  Widget _cameraTogglesRowWidget() {
+    final List<Widget> toggles = <Widget>[];
+
+    void onChanged(CameraDescription? description) {
+      if (description == null) {
+        return;
+      }
+
+      onNewCameraSelected(description);
+    }
+
+    if (cameras.isEmpty) {
+      return const Text('None');
+    } else {
+      for (final CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          SizedBox(
+            width: 90.0,
+            child: RadioListTile<CameraDescription>(
+              title: Icon(getCameraLensIcon(cameraDescription.lensDirection)),
+              groupValue: _controller!.description,
+              value: cameraDescription,
+              onChanged: _controller!.value.isRecordingVideo ? null : onChanged,
+            ),
+          ),
+        );
+      }
+    }
+
+    return Row(children: toggles);
+  }
+
+  IconData getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        throw ArgumentError('Unknown lens direction');
+    }
+  }
+
+  Future<void> onNewCameraSelected(CameraDescription cameraDescription) async {
+    final CameraController? oldController = _controller;
+    if (oldController != null) {
+      _controller = null;
+      await oldController.dispose();
+    }
+
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      kIsWeb ? ResolutionPreset.max : ResolutionPreset.medium,
+      imageFormatGroup: ImageFormatGroup.jpeg,
     );
+
+    _controller = cameraController;
+
+    // If the controller is updated then update the UI.
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+      if (cameraController.value.hasError) {
+        showInSnackBar(
+            'Camera error ${cameraController.value.errorDescription}');
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      switch (e.code) {
+        case 'CameraAccessDenied':
+          showInSnackBar('You have denied camera access.');
+          break;
+        case 'CameraAccessDeniedWithoutPrompt':
+          // iOS only
+          showInSnackBar('Please go to Settings app to enable camera access.');
+          break;
+        case 'CameraAccessRestricted':
+          // iOS only
+          showInSnackBar('Camera access is restricted.');
+          break;
+        case 'AudioAccessDenied':
+          showInSnackBar('You have denied audio access.');
+          break;
+        case 'AudioAccessDeniedWithoutPrompt':
+          // iOS only
+          showInSnackBar('Please go to Settings app to enable audio access.');
+          break;
+        case 'AudioAccessRestricted':
+          // iOS only
+          showInSnackBar('Audio access is restricted.');
+          break;
+        default:
+          _showCameraException(e);
+          break;
+      }
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _showCameraException(CameraException e) {
+    _logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+}
+
+void _logError(String code, String? message) {
+  if (message != null) {
+    print('Error: $code\nError Message: $message');
+  } else {
+    print('Error: $code');
   }
 }
